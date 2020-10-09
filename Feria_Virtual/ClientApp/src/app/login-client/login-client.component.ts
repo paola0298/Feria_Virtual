f9359f7256232aac2c9450fb2a1d0ae18715901f
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-10-08 19:47:13 -0600

    Agregado metodo para obtener el usuario y contraseña

--- Feria_Virtual/ClientApp/src/app/login-client/login-client.component.ts
+++ Feria_Virtual/ClientApp/src/app/login-client/login-client.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilsService } from 'src/app/shared/utils.service'
 
 @Component({
   selector: 'app-login-client',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginClientComponent implements OnInit {
 
+  utilsService:UtilsService = new UtilsService();
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  login() {
+    let username = (document.getElementById("username") as HTMLInputElement);
+    let pass = (document.getElementById("password") as HTMLInputElement);
+
+    console.log("Username: " + username.value + " Password: " + pass.value);
+
+    // TODO verificar que este registrado el usuario
+    if (username.value == "" || pass.value == "") {
+      this.utilsService.showInfoModal("Error", "Por favor complete todos los campos", "saveMsjLabel", "msjText", "saveMsj");
+    } else {
+      //cargar pagina
+    }
+  }
+
+  closeModal(modal:string) {
+    document.getElementById(modal).style.setProperty('display', 'none');
+
   }
 
 }
