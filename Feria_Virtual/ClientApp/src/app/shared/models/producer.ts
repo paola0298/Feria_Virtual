4d82123ed5798150009dae57b133aa5788d4804a
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-10-12 11:26:05 -0600

    Agregado modelo de productor

---
+++ Feria_Virtual/ClientApp/src/app/shared/models/producer.ts
@@ -0,0 +1,15 @@
+
+export class Producer {
+    id: number;
+    name: string;
+    sinpe: number;
+    lastName: string;
+    lastName2: string;
+    phone: number;
+    birth: string;
+    province: string;
+    canton: string;
+    district: string;
+    dir: string;
+    deliver: string[]; 
+}
