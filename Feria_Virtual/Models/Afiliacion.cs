82887f5dfab406a0497dfd89f0feced56d0d2690
Marlon Alejandro Vega <marlon.vega@xerasistemas.net>
2020-10-17 20:59:30 -0600

    Agregado atributo comentario faltante

--- Feria_Virtual/Models/Afiliacion.cs
+++ Feria_Virtual/Models/Afiliacion.cs
@@ -2,18 +2,22 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using Feria_Virtual.Models;
 
-namespace Feria_Virtual.Models {
+namespace Feria_Virtual.Models
+{
 
-    public enum AfiliacionStatus {
+    public enum AfiliacionStatus
+    {
         Pendiente = 0,
         Aceptada = 1,
         Denegada = 2
     }
 
-    public class Afiliacion {
+    public class Afiliacion
+    {
         [Key]
         public int Id { get; set; }
         public AfiliacionStatus Estado { get; set; }
         public string IdProductor { get; set; }
+        public string Comentario { get; set; }
     }
 }
