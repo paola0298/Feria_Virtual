5a5ed560b5ed6d1722f84b2a1841f776313eeaeb
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-10-17 21:41:51 -0600

    Creados metodos para conectarse al servicio y utilizar el api

--- Feria_Virtual/ClientApp/src/app/producer/producer.component.ts
+++ Feria_Virtual/ClientApp/src/app/producer/producer.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { UtilsService } from 'src/app/services/utils.service';
 import { Producer } from 'src/app/models/producer';
 import { RestclientService } from 'src/app/services/restclient.service';
-import { Observable } from 'rxjs';
 
 
 @Component({
@@ -18,38 +17,107 @@ export class ProducerComponent implements OnInit {
   producers = [];
   actualProducer: Producer;
   deliveryZones:string[] = [];
+  testProducer: Producer;
 
 
-  constructor(private restClientService: RestclientService, private utilsService: UtilsService) { }
+  constructor(private restClientService: RestclientService, private utilsService: UtilsService) { 
+    this.testProducer = new Producer("Paola", "4023900831", "Villegas", "Chacon", "123456789", "83216963", "1998-02-06", "Heredia", "Heredia", "Varablanca", ["Heredia"]);
+  }
 
   ngOnInit(): void {
     this.utilsService.configureContextMenu();
-
     //Cargar las provincias al iniciar el componente
     this.getProvinces();
-    
-    // this.restClientService.createProducer(new Producer(
-    //   "Marlon", "122200589521", "Vega", "Chinchilla", "123456789", "70143773", "1999-01-11", "San José", 
-    //   "Escazú", "San Antonio", "casa", ["san jose"]
-    // ))
-    // console.log(this.utilsService.getProducers());
-    console.log(this.restClientService.getProducers());
     this.loadProducers();
-    
-    
   }
 
-  async loadProducers() {
-    this.producers = await this.utilsService.getProducers();
-    // this.producers = this.restClientService.getProducers();
+  /**
+   * Metodo que se conecta con el servicio para crear un nuevo productor
+   * @param producer Productor a crear
+   * @param htmlElements Elementos html input para limpiar los campos
+   * @param selectElements Elementos html select para limpiar los campos
+   */
+  createProducer(producer:Producer, htmlElements:HTMLInputElement[], selectElements:HTMLSelectElement[]){
+    var response = this.restClientService.createProducer(producer);
+    response.subscribe(
+      (value:Producer) => {
+        console.log("Producer " + value);
+        this.loadProducers();
+        this.utilsService.showInfoModal("Exito", "Nueva productor guardado correctamente.", "saveMsjLabel", "msjText", 'saveMsj');
+        this.utilsService.cleanField(htmlElements,selectElements, 
+          ["Seleccione una provincia", "Seleccione un cantón", "Seleccione un distrito"]);
+        this.deliveryZones = [];
+      }, (error:any) => {
+        console.log(error.statusText);
+        console.log(error.status);
+        if (error.status == 409)
+          this.utilsService.showInfoModal("Error", "El productor ya existe", "saveMsjLabel", "msjText", 'saveMsj');
+      });
+  }
 
+  //Funciono: Ok, NoContent, Created
+  //No funciono: BadRequest, NotFound, Conflict
 
+  /**
+   * Metodo para conectrase al servicio para obtener todos los productores registrados
+   */
+  loadProducers() {
+    var response = this.restClientService.getProducers();
+    response.subscribe(
+      (value:Producer[]) => {
+        console.log("Producers " + value);
+        this.producers = value;
+      }, (error:any) => {
+        console.log(error.statusText);
+        console.log(error.status);
+      });
+  }
+
+  removeProducer(id:string) {
+    var response = this.restClientService.deleteProducer(id);
+    response.subscribe(
+      (value:any) => {
+        console.log("Deleted");
+        this.utilsService.showInfoModal("Exito", "Productor eliminado correctamente.", "saveMsjLabel", "msjText", 'saveMsj');
+        this.loadProducers();
+      }, (error:any) => {
+        console.log(error.statusText);
+        console.log(error.status);
+        this.utilsService.showInfoModal("Error", "Hubo un problema al eliminar al productor.", "saveMsjLabel", "msjText", 'saveMsj');
+      });
+  }
+
+  /**
+   * Metodo que se conecta con el servicio para actualizar un productor
+   * @param producer Productor a actualizar
+   * @param htmlElements Elementos html input para limpiar los campos
+   * @param selectElements Elementos html select para limpiar los campos
+   */
+  modifyProducer(producer, htmlElements:HTMLInputElement[], selectElements:HTMLSelectElement[]) {
+    var response = this.restClientService.updateProducer(producer);
+    response.subscribe(
+      (value:any) => {
+        console.log("Updated");
+        this.utilsService.showInfoModal("Exito", "Productor actualizado correctamente.", "saveMsjLabel", "msjText", 'saveMsj');
+        document.getElementById("idProducer").removeAttribute('disabled');
+        this.utilsService.cleanField(htmlElements,selectElements, ["Seleccione una provincia", "Seleccione un cantón", "Seleccione un distrito"]);
+        this.deliveryZones = [];
+        this.loadProducers();
+        this.updating = false;
+        (document.getElementById("saveButton") as HTMLButtonElement).textContent = "Guardar nuevo productor";
+      }, (error:any) => {
+        console.log(error.statusText);
+        console.log(error.status);
+        this.utilsService.showInfoModal("Error", "Hubo un problema al actualizar al productor.", "saveMsjLabel", "msjText", 'saveMsj');
+      }
+    );
   }
 
   /**
    * Metodo para almacenar el nuevo productor
    */
-  saveProducer(): void {
+  async saveProducer() {
+
     let id = (document.getElementById("idProducer") as HTMLInputElement);
     let name = (document.getElementById("name") as HTMLInputElement);
     let lastName1 = (document.getElementById("last-name1") as HTMLInputElement);
@@ -60,32 +128,23 @@ export class ProducerComponent implements OnInit {
     let province = (document.getElementById("province") as HTMLSelectElement);
     let canton = (document.getElementById("canton") as HTMLSelectElement);
     let district = (document.getElementById("district") as HTMLSelectElement);
-    let deliver = (document.getElementById("deliver") as HTMLInputElement);
     
     // TODO verificar que se haya ingresado alguna provincia, canton y distrito
     if (id.value == '' || name.value == '' || lastName1.value == '' || lastName2.value == '' || sinpe.value == '' || phone.value == '' || birth.value == '' || 
     this.deliveryZones.length == 0) {
         this.utilsService.showInfoModal("Error", "Por favor complete todos los campos.", "saveMsjLabel", "msjText", 'saveMsj');
+        return;
+    }
+
+    var producer = new Producer(name.value, id.value, lastName1.value, lastName2.value, 
+      sinpe.value, phone.value, birth.value, province.value, canton.value, district.value,
+      this.deliveryZones);
+    
+    if (this.updating) {
+      producer.afiliado = true;
+      this.modifyProducer(producer, [id, name, lastName1, lastName2, sinpe, phone, birth], [province, canton, district]);
     } else {
-      var producer:Producer = new Producer(name.value, id.value, lastName1.value, lastName2.value, 
-        sinpe.value, phone.value, birth.value, province.value, canton.value, district.value,
-        this.deliveryZones);
-      
-
-      if (this.updating) {
-        this.utilsService.showInfoModal("Exito", "Productor actualizado correctamente.", "saveMsjLabel", "msjText", 'saveMsj');
-        let indexProducer = this.producers.indexOf(this.actualProducer);
-        this.producers[indexProducer] = producer;
-        this.updating = false;
-        document.getElementById("idProducer").removeAttribute('disabled');
-
-      } else {
-        this.utilsService.showInfoModal("Exito", "Nueva productor guardado correctamente.", "saveMsjLabel", "msjText", 'saveMsj');
-        this.producers.push(producer);
-      }
-      this.utilsService.cleanField([id, name, lastName1, lastName2, sinpe, phone, birth],
-        [province, canton, district], ["Seleccione una provincia", "Seleccione un cantón", "Seleccione un distrito"]);
-      this.deliveryZones = [];
+      this.createProducer(producer, [id, name, lastName1, lastName2, sinpe, phone, birth], [province, canton, district]);
     }
   }
 
@@ -93,13 +152,12 @@ export class ProducerComponent implements OnInit {
    * Metodo para actualizar la informacion de un productor
    */
   updateProducer(): void {
-
-
+    this.updating = true;
+    (document.getElementById("saveButton") as HTMLButtonElement).textContent = "Actualizar";
     this.loadCanton(this.actualProducer.provincia);
     this.loadDistrict(this.actualProducer.canton);
 
     document.getElementById("idProducer").setAttribute('disabled', 'true');
-    this.updating = true;
     // Cargar los datos del productor en el formulario y deshabilitar el campo de id
 
     var birthDateTime = this.actualProducer.fechaNacimiento;
@@ -119,16 +177,17 @@ export class ProducerComponent implements OnInit {
     (document.getElementById('district') as HTMLSelectElement).value = this.actualProducer.distrito;  
 
     this.deliveryZones = this.actualProducer.lugaresEntrega;
-    // console.log(document.getElementById("province").getAttribute("selected"));
   }
 
   /**
    * Metodo para eliminar un productor
    */
   deleteProducer(): void {
+    this.removeProducer(this.actualProducer.identificacion);
     document.getElementById('optionMsj').style.setProperty('display', 'none');
-    const index = this.producers.indexOf(this.actualProducer, 0);
-    this.producers.splice(index, 1);
+    // this.loadProducers();
+    // const index = this.producers.indexOf(this.actualProducer, 0);
+    // this.producers.splice(index, 1);
   }
 
   /**
@@ -136,7 +195,6 @@ export class ProducerComponent implements OnInit {
    */
   async getProvinces() {
     this.provinces = await this.utilsService.getProvinces();
-    console.log(this.provinces);
   }
 
   /**
