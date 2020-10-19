d7eceb5f9df5f46a932b1162ee69a55e54ce588f
Marlon Alejandro Vega <marlon.vega@xerasistemas.net>
2020-10-18 21:06:51 -0600

    Eliminado modelo sin uso

--- Feria_Virtual/Models/ProductoCarrito.cs
+++
@@ -1,9 +0,0 @@
-namespace Feria_Virtual.Models
-{
-    public class ProductoCarrito
-    {
-        public string IdCliente { get; set; }
-        public int IdProducto { get; set; }
-        public int Cantidad { get; set; }
-    }
-}
