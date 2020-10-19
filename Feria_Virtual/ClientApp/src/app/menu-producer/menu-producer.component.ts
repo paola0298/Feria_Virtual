e0c786f0eeab21e67fdb5db1c0d2cc54973e3820
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-10-18 18:32:52 -0600

    Creados componentes de menu admin, client, producer

---
+++ Feria_Virtual/ClientApp/src/app/menu-producer/menu-producer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-producer',
+  templateUrl: './menu-producer.component.html',
+  styleUrls: ['./menu-producer.component.css']
+})
+export class MenuProducerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
