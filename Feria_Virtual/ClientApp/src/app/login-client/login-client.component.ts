d1a3678bc57d4ba456b2b8253fc2c33dc60759f4
Paola Villegas Chacón <37082461+paola0298@users.noreply.github.com>
2020-10-18 23:05:27 -0600

    Conectado el login con el backend

--- Feria_Virtual/ClientApp/src/app/login-client/login-client.component.ts
+++ Feria_Virtual/ClientApp/src/app/login-client/login-client.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UtilsService } from 'src/app/services/utils.service'
+import { RestclientService } from 'src/app/services/restclient.service'
+import { Router } from '@angular/router'; 
+
 
 @Component({
   selector: 'app-login-client',
@@ -9,21 +12,44 @@ import { UtilsService } from 'src/app/services/utils.service'
 export class LoginClientComponent implements OnInit {
 
   
-  constructor(private utilsService:UtilsService) { }
+  constructor(private utilsService:UtilsService, private restClientService: RestclientService,
+    private router: Router) { }
 
   ngOnInit() { }
+
+  checkLogin(id:string, pass:string) {
+    var response = this.restClientService.loginClient(id, pass);
+    response.subscribe(
+      (value:any) => {
+        //cargar pagina
+        var userId = value.id;
+        var client:boolean = value.client;
+
+        console.log(userId + " " + client);
+
+        if (client) {
+          window.localStorage.setItem("userId", userId);
+          this.router.navigate(['menu-client']);
+        } else {
+          this.utilsService.showInfoModal("Error", "Error al iniciar sesion, el cliente no esta registrado", "saveMsjLabel", "msjText", 'saveMsj');
+        }
+      }, (error:any) => {
+        console.log(error.statusText);
+        console.log(error.status);
+        this.utilsService.showInfoModal("Error", "Error al iniciar sesion", "saveMsjLabel", "msjText", 'saveMsj');
+      });
+  }
 
   login() {
     let username = (document.getElementById("username") as HTMLInputElement);
     let pass = (document.getElementById("password") as HTMLInputElement);
 
     console.log("Username: " + username.value + " Password: " + pass.value);
-
     // TODO verificar que este registrado el usuario
     if (username.value == "" || pass.value == "") {
       this.utilsService.showInfoModal("Error", "Por favor complete todos los campos", "saveMsjLabel", "msjText", "saveMsj");
     } else {
-      //cargar pagina
+      this.checkLogin(username.value, pass.value);
     }
   }
 
