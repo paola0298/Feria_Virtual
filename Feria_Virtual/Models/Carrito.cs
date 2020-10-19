8d057e8978f431e8861d924ececc64b70c434640
Marlon Alejandro Vega <marlon.vega@xerasistemas.net>
2020-10-18 21:06:10 -0600

    Modificado controlador y modelo del carrito

---
+++ Feria_Virtual/Models/Carrito.cs
@@ -0,0 +1,9 @@
+namespace Feria_Virtual.Models
+{
+    public class Carrito
+    {
+        public string IdCliente { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
