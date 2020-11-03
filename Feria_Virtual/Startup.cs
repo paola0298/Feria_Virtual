03036ea10e5b560626331d74d8e7e47fcf9396a4
Marlon Vega <mvegach2013@gmail.com>
2020-11-03 10:03:48 -0600

    Habilitado Cors

--- Feria_Virtual/Startup.cs
+++ Feria_Virtual/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace Feria_Virtual
 {
@@ -23,6 +24,8 @@ namespace Feria_Virtual
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -66,6 +69,8 @@ namespace Feria_Virtual
             {
                 app.UseSpaStaticFiles();
             }
+
+            app.UseCors(options => options.WithOrigins("*").AllowAnyMethod().AllowAnyHeader());
 
             app.UseRouting();
 
